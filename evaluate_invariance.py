3c0f348454618adaa843912f6a49b1e63690aaab
Yohanes Gultom <yohanes.gultom@gmail.com>
2018-03-21 13:53:26 +0700

    update evaluate_invariance

--- evaluate_invariance.py
+++ evaluate_invariance.py
@@ -1,78 +1,90 @@
 import argparse
 import extractor as vgg16_extractor
-import evaluate_sklearn
 import pickle
 import numpy as np
-from helper import read_and_transform_dataset, extract_bow_features
+from helper import build_dataset_vgg16, build_dataset_sklearn
 from sklearn.metrics import accuracy_score
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Rotate and zoom images and test them with VGG16, SIFT and SURF classifiers', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('test_dir_path', help="Path to raw test dataset directory")
-    parser.add_argument('--vgg16_model', help="VGG16 classifier .pkl")
-    parser.add_argument('--sift_dict', help="SIFT BoW dictionary .pkl")
-    parser.add_argument('--sift_model', help="SIFT scikit-learn classifier .pkl")
-    parser.add_argument('--surf_dict', help="SURF BoW dictionary .pkl")
-    parser.add_argument('--surf_model', help="SURF scikit-learn classifier .pkl")
+	parser = argparse.ArgumentParser(description='Rotate and zoom images and test them with VGG16, SIFT and SURF classifiers', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser.add_argument('test_dir_path', help="Path to raw test dataset directory")
+	parser.add_argument('--vgg16_model', help="VGG16 classifier .pkl")
+	parser.add_argument('--sift_dict', help="SIFT BoW dictionary .pkl")
+	parser.add_argument('--sift_model', help="SIFT scikit-learn classifier .pkl")
+	parser.add_argument('--surf_dict', help="SURF BoW dictionary .pkl")
+	parser.add_argument('--surf_model', help="SURF scikit-learn classifier .pkl")
 
-    args = parser.parse_args()
-    test_dir_path = args.test_dir_path
-    vgg16_model = args.vgg16_model	
-    sift_dict = args.sift_dict	
-    sift_model = args.sift_model	
-    surf_dict = args.surf_dict	
-    surf_model = args.surf_model	
+	args = parser.parse_args()
+	test_dir_path = args.test_dir_path
+	vgg16_model = args.vgg16_model	
+	sift_dict = args.sift_dict	
+	sift_model = args.sift_model	
+	surf_dict = args.surf_dict	
+	surf_model = args.surf_model	
 
-    experiments = {
-        'rotate': [90, 180, 270],
-        'scale': [10, 30, 50],
-    }
+	experiments = {
+		'rotate': [90, 180, 270],
+		'scale': [10, 30, 50],
+	}
 
-    X = None
-    y = None
-    results = []
-    for exp, values in experiments.iteritems():
-        for v in values:
-            # transform dataset
-            if exp.lower() == 'rotate':
-                X, y = read_and_transform_dataset(test_dir_path, rotation=v)
-            elif exp.lower() == 'scale':
-                X, y = read_and_transform_dataset(test_dir_path, scale=1 + v / 100.0)
+	X = None
+	y = None
+	X_f = None
+	results = []    
 
-            if vgg16_model is not None:
-                model = pickle.load(open(vgg16_model, 'rb'))
-                extractor = vgg16_extractor.build_extractor(vgg16_extractor.EXPECTED_DIM)
-                X_f = extractor.predict(X, verbose=0)
-                accuracy = model.score(X_f, y)
-                results.append(('VGG16 + MLP', exp, v, accuracy))
+	# get test data info
+	dir_names, file_paths, file_dir_indexes = get_dir_info(train_dir_path)
 
-            if sift_dict is not None and sift_model is not None:
-                sift = cv2.xfeatures2d.SIFT_create()
-                dictionary = pickle.load(open(sift_dict, 'rb'))
-                model = pickle.load(open(sift_model, 'rb'))
-                X_f = []
-                for x in X:
-                    kp, dsc = sift.detectAndCompute(x, None)
-                    features = extract_bow_features(dsc, dictionary)
-                    X_f.append(features)
-                X_f = np.array(X_f)
-                accuracy = model.score(X_f, y)
-                results.append(('SIFT + MLP', exp, v, accuracy))
+	# evaluate vgg16 extractor
+	if vgg16_model is not None:
+		model = pickle.load(open(vgg16_model, 'rb'))
+		extractor = vgg16_extractor.build_extractor(vgg16_extractor.EXPECTED_DIM)
 
-            if surf_dict is not None and surf_model is not None:
-                surf = cv2.xfeatures2d.SURF_create()
-                dictionary = pickle.load(open(surf_dict, 'rb'))
-                model = pickle.load(open(surf_model, 'rb'))
-                X_f = []
-                for x in X:
-                    kp, dsc = surf.detectAndCompute(x, None)
-                    features = extract_bow_features(dsc, dictionary)
-                    X_f.append(features)
-                X_f = np.array(X_f)
-                accuracy = model.score(X_f, y)
-                results.append(('SURF + MLP', exp, v, accuracy))
-    
-    print('\n\nResults:')
-    for r in results:
-        print('\t'.join([ str(v) for v in r ]))
+		for exp, values in experiments.iteritems():
+			for v in values:
+				if exp.lower() == 'rotate':
+					X, y = build_dataset_vgg16(dir_names, file_paths, file_dir_indexes, extractor, expected_size=vgg16_extractor.EXPECTED_SIZE, rotation=v)
+				elif exp.lower() == 'scale':
+					X, y = build_dataset_vgg16(dir_names, file_paths, file_dir_indexes, extractor, expected_size=vgg16_extractor.EXPECTED_SIZE, scale=1 + v / 100.0)
+
+				accuracy = model.score(X, y)
+				results.append(('VGG16 + MLP', exp, v, accuracy))
+
+
+	# evaluate sift extractor
+	if sift_dict is not None and sift_model is not None:
+		sift = cv2.xfeatures2d.SIFT_create()
+		dictionary = pickle.load(open(sift_dict, 'rb'))
+		model = pickle.load(open(sift_model, 'rb'))
+
+		for exp, values in experiments.iteritems():
+			for v in values:
+				if exp.lower() == 'rotate':
+					X, y = build_dataset_sklearn(dir_names, file_paths, file_dir_indexes, dictionary, sift, rotation=v)
+				elif exp.lower() == 'scale':
+					X, y = build_dataset_sklearn(dir_names, file_paths, file_dir_indexes, dictionary, sift, scale=1 + v / 100.0)
+
+				accuracy = model.score(X, y)
+				results.append(('SIFT + MLP', exp, v, accuracy))
+
+
+	# evaluate surf extractor
+	if surf_dict is not None and surf_model is not None:
+		surf = cv2.xfeatures2d.SURF_create()
+		dictionary = pickle.load(open(surf_dict, 'rb'))
+		model = pickle.load(open(surf_model, 'rb'))
+		for exp, values in experiments.iteritems():
+			for v in values:
+				if exp.lower() == 'rotate':
+					X, y = build_dataset_sklearn(dir_names, file_paths, file_dir_indexes, dictionary, surf, rotation=v)
+				elif exp.lower() == 'scale':
+					X, y = build_dataset_sklearn(dir_names, file_paths, file_dir_indexes, dictionary, surf, scale=1 + v / 100.0)
+
+				accuracy = model.score(X, y)
+				results.append(('SURF + MLP', exp, v, accuracy))
+
+
+	print('\n\nResults:')
+	for r in results:
+		print('\t'.join([ str(v) for v in r ]))
--- helper.py
+++ helper.py
@@ -13,113 +13,94 @@ import imutils
 from sklearn.cluster import MiniBatchKMeans
 
 def get_dir_info(dir_path):
-    dir_names = os.listdir(dir_path)
-    file_paths = []
-    file_dir_indexes = []
+	dir_names = os.listdir(dir_path)
+	file_paths = []
+	file_dir_indexes = []
 
-    for i in range(len(dir_names)):
-        p = dir_names[i]
-        subdir = os.path.join(dir_path, p)
-        files = os.listdir(subdir)
-        for f in files:
-            file_paths.append(os.path.join(subdir, f))
-            file_dir_indexes.append(i)
-    
-    return dir_names, file_paths, file_dir_indexes
+	for i in range(len(dir_names)):
+		p = dir_names[i]
+		subdir = os.path.join(dir_path, p)
+		files = os.listdir(subdir)
+		for f in files:
+			file_paths.append(os.path.join(subdir, f))
+			file_dir_indexes.append(i)
+	
+	return dir_names, file_paths, file_dir_indexes
 
 def build_extractor(xfeatures2d, dir_names=None, file_paths=None, dictionary_size=None, dictionary=None):
-    if dir_names is not None and file_paths is not None:
-        print('Computing descriptors..')        
-        BOW = cv2.BOWKMeansTrainer(dictionary_size)
-        num_files = len(file_paths)
-        bar = progressbar.ProgressBar(maxval=num_files).start()
-        for i in range(num_files):
-            p = file_paths[i]
-            image = cv2.imread(p)
-            gray = cv2.cvtColor(image, cv2.cv2.IMREAD_GRAYSCALE)
-            kp, dsc = xfeatures2d.detectAndCompute(gray, None)
-            BOW.add(dsc)
-            bar.update(i)
-        bar.finish()
+	if dir_names is not None and file_paths is not None:
+		print('Computing descriptors..')        
+		BOW = cv2.BOWKMeansTrainer(dictionary_size)
+		num_files = len(file_paths)
+		bar = progressbar.ProgressBar(maxval=num_files).start()
+		for i in range(num_files):
+			p = file_paths[i]
+			image = cv2.imread(p)
+			gray = cv2.cvtColor(image, cv2.cv2.IMREAD_GRAYSCALE)
+			kp, dsc = xfeatures2d.detectAndCompute(gray, None)
+			BOW.add(dsc)
+			bar.update(i)
+		bar.finish()
 
-        print('Creating BoW vocabs using K-Means clustering with k={}..'.format(dictionary_size))
-        dictionary = BOW.cluster()
+		print('Creating BoW vocabs using K-Means clustering with k={}..'.format(dictionary_size))
+		dictionary = BOW.cluster()
 
-    if dictionary is not None:        
-        print "bow dictionary", np.shape(dictionary)
-        extractor = cv2.BOWImgDescriptorExtractor(xfeatures2d, cv2.BFMatcher(cv2.NORM_L2))
-        extractor.setVocabulary(dictionary)
-    return extractor, dictionary
+	if dictionary is not None:        
+		print "bow dictionary", np.shape(dictionary)
+		extractor = cv2.BOWImgDescriptorExtractor(xfeatures2d, cv2.BFMatcher(cv2.NORM_L2))
+		extractor.setVocabulary(dictionary)
+	return extractor, dictionary
 
 
 def build_dataset(dir_names, file_paths, file_dir_indexes, extractor, xfeatures2d, rotation=0, scale=1):
-    assert len(file_paths) == len(file_dir_indexes)    
-    X = [] # data
-    y = file_dir_indexes # labels
+	assert len(file_paths) == len(file_dir_indexes)    
+	X = [] # data
+	y = file_dir_indexes # labels
 
-    print('Computing features using Bag-of-Words dictionary..')
-    num_files = len(file_paths)
-    bar = progressbar.ProgressBar(maxval=num_files).start()
-    for i in range(num_files):
-        p = file_paths[i]
-        im = cv2.imread(p, 1)
-        im = imutils.rotate(im, rotation) if rotation > 0 else im
-        im = zoomin(im, scale) if scale > 1 else im
-        gray = cv2.cvtColor(im, cv2.IMREAD_GRAYSCALE)
-        feature = extractor.compute(gray, xfeatures2d.detect(gray))
-        X.extend(feature)
-        bar.update(i)
-    bar.finish()
-    return X, y
+	print('Computing features using Bag-of-Words dictionary..')
+	num_files = len(file_paths)
+	bar = progressbar.ProgressBar(maxval=num_files).start()
+	for i in range(num_files):
+		p = file_paths[i]
+		im = cv2.imread(p, 1)
+		im = imutils.rotate(im, rotation) if rotation > 0 else im
+		im = zoomin(im, scale) if scale > 1 else im
+		gray = cv2.cvtColor(im, cv2.IMREAD_GRAYSCALE)
+		feature = extractor.compute(gray, xfeatures2d.detect(gray))
+		X.extend(feature)
+		bar.update(i)
+	bar.finish()
+	return X, y
 
 
 def build_dictionary(xfeatures2d, dir_names, file_paths, dictionary_size):
-    print('Computing descriptors..')        
-    desc_list = []
-    num_files = len(file_paths)
-    bar = progressbar.ProgressBar(maxval=num_files).start()
-    for i in range(num_files):
-        p = file_paths[i]
-        image = cv2.imread(p)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        kp, dsc = xfeatures2d.detectAndCompute(gray, None)
-        desc_list.extend(dsc)
-        bar.update(i)
-    bar.finish()
+	print('Computing descriptors..')        
+	desc_list = []
+	num_files = len(file_paths)
+	bar = progressbar.ProgressBar(maxval=num_files).start()
+	for i in range(num_files):
+		p = file_paths[i]
+		image = cv2.imread(p)
+		gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+		kp, dsc = xfeatures2d.detectAndCompute(gray, None)
+		desc_list.extend(dsc)
+		bar.update(i)
+	bar.finish()
 
-    print('Creating BoW dictionary using K-Means clustering with k={}..'.format(dictionary_size))
-    dictionary = MiniBatchKMeans(n_clusters=dictionary_size, batch_size=100, verbose=1)
-    dictionary.fit(np.array(desc_list))
-    return dictionary
+	print('Creating BoW dictionary using K-Means clustering with k={}..'.format(dictionary_size))
+	dictionary = MiniBatchKMeans(n_clusters=dictionary_size, batch_size=100, verbose=1)
+	dictionary.fit(np.array(desc_list))
+	return dictionary
 
 def extract_bow_features(descriptors, dictionary):
-    features = np.zeros(dictionary.n_clusters)
-    predictions = dictionary.predict(descriptors)
-    for p in predictions:
-        features[p] += 1
-    return features
-
-def build_dataset_sklearn(dir_names, file_paths, file_dir_indexes, dictionary, xfeatures2d):
-    assert len(file_paths) == len(file_dir_indexes)    
-    X = [] # data
-    y = file_dir_indexes # labels
-
-    print('Computing features using Bag-of-Words dictionary..')
-    num_files = len(file_paths)
-    bar = progressbar.ProgressBar(maxval=num_files).start()
-    for i in range(num_files):
-        p = file_paths[i]
-        im = cv2.imread(p, 1)
-        gray = cv2.cvtColor(im, cv2.IMREAD_GRAYSCALE)
-        kp, dsc = xfeatures2d.detectAndCompute(gray, None)
-        features = extract_bow_features(dsc, dictionary)
-        X.append(features)
-        bar.update(i)
-    bar.finish()
-    return X, y
+	features = np.zeros(dictionary.n_clusters)
+	predictions = dictionary.predict(descriptors)
+	for p in predictions:
+		features[p] += 1
+	return features
 
 def resize(data, size):
-    return cv2.resize(data, (size, size))
+	return cv2.resize(data, (size, size))
 
 def zoomin(source, z):
 	if z < 1:
@@ -133,23 +114,46 @@ def zoomin(source, z):
 	return cropped
 
 
-def read_and_transform_dataset(dir_path, rotation=0, scale=1):
-    X = [] # data
-    y = [] # labels
+def build_dataset_sklearn(dir_names, file_paths, file_dir_indexes, dictionary, xfeatures2d, rotation=0, scale=1):
+	assert len(file_paths) == len(file_dir_indexes)    
+	X = [] # data
+	y = file_dir_indexes # labels
 
-    print('Reading images..')
-    dir_names = os.listdir(dir_path)
-    for i in range(len(dir_names)):
-        p = dir_names[i]
-        subdir = os.path.join(dir_path, p)
-        for f in os.listdir(subdir):
-            file_path = os.path.join(subdir, f)
-            im = cv2.imread(file_path, 1)
-            im = cv2.cvtColor(im, cv2.IMREAD_GRAYSCALE)
-            im = imutils.rotate(im, rotation) if rotation > 0 else im
-            im = zoomin(im, scale) if scale > 1 else im
-            X.append(im)
-            y.append(i)
+	print('Computing features using Bag-of-Words dictionary..')
+	num_files = len(file_paths)
+	bar = progressbar.ProgressBar(maxval=num_files).start()
+	for i in range(num_files):
+		p = file_paths[i]
+		im = cv2.imread(p, 1)
+		im = cv2.cvtColor(im, cv2.IMREAD_GRAYSCALE)
+		im = imutils.rotate(im, rotation) if rotation > 0 else im
+		im = zoomin(im, scale) if scale > 1 else im            
+		kp, dsc = xfeatures2d.detectAndCompute(im, None)
+		features = extract_bow_features(dsc, dictionary)
+		X.append(features)
+		bar.update(i)
+	bar.finish()
+	return np.array(X), np.array(y)
 
-    return np.array(X), np.array(y)
+
+def build_dataset_vgg16(dir_names, file_paths, file_dir_indexes, extractor, expected_size=224, rotation=0, scale=1):
+	assert len(file_paths) == len(file_dir_indexes)    
+	X = [] # data
+	y = np.array(file_dir_indexes) # labels
+
+	print('Computing features using Bag-of-Words dictionary..')
+	num_files = len(file_paths)
+	bar = progressbar.ProgressBar(maxval=num_files).start()
+	for i in range(num_files):
+		p = file_paths[i]
+		im = cv2.imread(p, 1)
+		im = cv2.cvtColor(im, cv2.IMREAD_GRAYSCALE)
+		im = imutils.rotate(im, rotation) if rotation > 0 else im
+		im = zoomin(im, scale) if scale > 1 else im            
+		vector = resize(im, expected_size)
+		X.append(vector)
+		bar.update(i)
+	bar.finish()
+	X = extractor.predict(np.array(X))
+	return X, y
 
